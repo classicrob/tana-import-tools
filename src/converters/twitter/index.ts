71f82ed7e0a9acc75fa5bfc560ef4548bdf0e04b
TanaRob <46608303+classicrob@users.noreply.github.com>
2023-01-24 10:37:29 -0800

    added create tweet and create field functions

--- src/converters/twitter/index.ts
+++ src/converters/twitter/index.ts
@@ -9,21 +9,22 @@ interface Tweet {
     id_str: string;
     favorite_count: string;
     entities: {
-      hashtags: [ ],
-      symbols: [ ],
+      hashtags: [];
+      symbols: [];
       user_mentions: [
+        // there may be more than one user mentioned. How to handle this?
         {
           name: string;
-          screen_name: "colmobrogain",
-          id_str: "32342718",
-          id: "32342718"
-        }
-      ],
-      urls: [ ]
-    },
+          screen_name: string;
+          id_str: string;
+        },
+      ];
+      urls: [];
+    };
     in_reply_to_status_id_str: string;
-  }
+  };
 }
+
 function convertTwitterDate(date: string): string {
   const dateObject = new Date(date);
   const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
@@ -31,49 +32,113 @@ function convertTwitterDate(date: string): string {
   const year = dateObject.getFullYear();
   return `${month}-${day}-${year}`;
 }
+
+function createDateNode(date: string): TanaIntermediateNode {
+  return {
+    uid: convertTwitterDate(date),
+    name: convertTwitterDate(date),
+    type: 'date',
+    createdAt: new Date(date).getTime(),
+    editedAt: new Date(date).getTime(),
+  };
+}
+function createField(name: string, value: string | string [])
+{
+  // Array.isArray is a built in function that returns true if the value is an array
+  // if it is an array, take the value as is. If not, make it an array with one element.
+  // Ask if I really need to handle things in separate branches or if I can coerce into the same format
+  const valueArray = Array.isArray(value) ? value : [value];
+  return {
+    uid: idgenerator(),
+    name: name,
+    createdAt: new Date().getTime(),
+    editedAt: new Date().getTime(),
+    type: 'field',
+    children: //map value array into an array of nodes
+      valueArray.map((value) => {
+        return {
+          uid: idgenerator(),
+          name: value,
+          createdAt: new Date().getTime(),
+          editedAt: new Date().getTime(),
+          type: 'node',
+        };
+      }),
+  };
+
+function createTweetNode(tweet: Tweet): TanaIntermediateNode {
+  const tweetTime = new Date(tweet.tweet.created_at).getTime();
+  return {
+    uid: idgenerator(),
+    name: tweet.tweet.full_text,
+    createdAt: tweetTime,
+    editedAt: tweetTime,
+    type: 'node',
+    children: [
+      {
+        uid: idgenerator(),
+        name: 'Author',
+        createdAt: tweetTime,
+        editedAt: tweetTime,
+        type: 'field',
+        children: [
+          {
+            uid: 'RobHaisfield',
+            name: 'Rob Haisfield', //how do I ensure that it only creates and references one Rob Haisfield node?
+            createdAt: tweetTime,
+            editedAt: tweetTime,
+            type: 'node',
+          },
+        ],
+      },
+      {
+        uid: idgenerator(),
+        name: 'Date',
+        createdAt: tweetTime,
+        editedAt: tweetTime,
+        type: 'field',
+        children: [createDateNode(tweet.tweet.created_at)],
+      },
+    ],
+  };
+}
 export class TwitterConverter {
-// returns a triple with version, summary, and nodes
-// name of the function, 0 or more arguments with their types, then the return type.
-// The bar undefined means it's a union of the TanaIntermediateFile and undefined. Could return either.
+  // returns a triple with version, summary, and nodes
+  // name of the function, 0 or more arguments with their types, then the return type.
+  // The bar undefined means it's a union of the TanaIntermediateFile and undefined. Could return either.
   convert(fileContent: string): TanaIntermediateFile | undefined {
     const json = JSON.parse(fileContent);
     const summary: TanaIntermediateSummary = {
-    leafNodes: 0,
-    topLevelNodes: json.length,
-    totalNodes: 0,
-    calendarNodes: 0,
-    fields: 0,
-    brokenRefs: 0,}
+      leafNodes: 0,
+      topLevelNodes: json.length,
+      totalNodes: 0,
+      calendarNodes: 0,
+      fields: 0,
+      brokenRefs: 0,
+    };
     //trying to make a map
-    const nodes = json.map((tweetObject: Tweet) => {
-      return (
-        {uid: idgenerator(),
-        name: tweetObject.tweet.full_text,
-        createdAt: new Date (tweetObject.tweet.created_at).getTime(),
-        editedAt: new Date (tweetObject.tweet.created_at).getTime(),
-        type: "node",
-        children: [{
-          uid: idgenerator(),
-          name: "Date",
-          createdAt: 0,
-          editedAt: 0,
-          type: "field",
-          children: [{
-            uid: idgenerator(),
-            name: convertTwitterDate(tweetObject.tweet.created_at),
-            createdAt: 0,
-            editedAt: 0,
-            type: "date",
-          }]
-        }]
-      })
-    })
-// rootLevelNodes are the first nodes in any thread. Date is in a field, not on day node.
+    const attributes = [
+      {
+        name: 'Author',
+        values: ['[[Rob Haisfield]]'],
+        count: 1,
+      },
+      {
+        name: 'Date',
+        values: [],
+        count: 1,
+      },
+    ];
+    const nodes = json.map(createTweetNode);
+    // Map takes a function reference, and applies it to each element in the array. If I called the function, then the outcome of that function is what's used.
+    // const nodes = json.map((tweetObject: Tweet) => createTweetNode(tweetObject)); this is passing an anonymous lambda function, equivalent to above
+    // rootLevelNodes are the first nodes in any thread. Date is in a field, not on day node.
 
     return {
       version: 'TanaIntermediateFile V0.1',
       summary,
       nodes,
+      attributes,
     };
   }
 }
